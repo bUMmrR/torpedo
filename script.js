2a5a1b3e36927e7fb9f64fd035515b807b06d6f0
Agócs Dániel <agocs.daniel@ckik.loc>
2023-04-25 13:27:02 +0200

    orai munka

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function generalas() { 
+const cim = document.getElementById("cim")
+
+
+function generalas(embere) { 
     const betuk = ["A","B","C","D","E","F","G","H","I","J"];
-    jatekter = document.getElementById("jatekter");
+    jatekter = document.getElementById("jatekter-"+embere);
     oszlop = 11;
     sor = 11;
     let table = document.createElement("table");
@@ -9,18 +12,24 @@ function generalas() {
         for (let j = 0; j < oszlop; j++) { 
             let td = document.createElement("td");
 
-            td.setAttribute("onclick", "Katt(this)")
+            if (i !=0 && j != 0) {
+                td.setAttribute("onclick", "Katt(this)")
+                
+            }
             td.dataset.sor = i;
             td.dataset.oszlop = j;
+            td.dataset.tabla = embere;
             if(j == 0){
                 if(i != 0){
 
                     td.innerHTML = i;
+                    td.setAttribute("onclick","")
                 }
             }
             
             if(i == 0){
                 if(betuk[j-1] != undefined){
+                    td.setAttribute("onclick","")
 
                     td.innerHTML = betuk[j-1];
                 }
@@ -31,6 +40,19 @@ function generalas() {
         table.appendChild(tr);
     }
     console.log()
+    cim.innerHTML="Hajók lerakása"
     jatekter.appendChild(table);
 }
-generalas();
+
+function Katt(td){
+    cim.innerHTML = "lövés";
+    td.style.backgroundColor = "var(--marlott)";
+    var kep = document.createElement("img")
+    kep.src = "explo.png";
+    // kep.height = 50;
+    // kep.width = 50;
+    td.appendChild(kep);
+    td.setAttribute("onclick","")
+}
+generalas("ember");
+generalas("bot")
